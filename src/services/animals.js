ea4bfcb4d6113088b0519226c08a3992b3840f27
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-29 14:35:36 -0700

    added getch by id to animals services

--- src/services/animals.js
+++ src/services/animals.js
@@ -1,6 +1,12 @@
-export default async function fetchAnimals() {
-  const url = 'http://localhost:7890/api/v1/animals';
+const url = 'http://localhost:7890/api/v1/animals';
+export async function fetchAnimals() {
   const data = await fetch(url);
+  const response = await data.json();
+  return response;
+}
+
+export async function fetchAnimalById(id) {
+  const data = await fetch(url + `/${id}`);
   const response = await data.json();
   return response;
 }
